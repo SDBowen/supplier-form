9e633b68542dc8389f23f5439ffebd29d7d4f09a
sdbowen <sdbowen.git@gmail.com>
2018-04-30 19:04:24 -0500

    Add scoping function

--- SupplierForm.js
+++ SupplierForm.js
@@ -1,176 +1,172 @@
 // Designed for use as a SharePoint WebPart
 //<script language="javascript" type="text/javascript">
-var webUrl =
-  window.location.protocol +
-  '//' +
-  window.location.host +
-  _spPageContextInfo.webServerRelativeUrl;
+(function() {
+  const webUrl =
+    window.location.protocol +
+    '//' +
+    window.location.host +
+    _spPageContextInfo.webServerRelativeUrl;
 
-// Add form submit event listener
-document.getElementById('supplierSubmit').addEventListener('click', formSubmit);
+  // Add form submit event listener
+  document
+    .getElementById('supplierSubmit')
+    .addEventListener('click', formSubmit);
 
-// Create SharePoint Document Set on form submit
-function formSubmit() {
+  // Create SharePoint Document Set on form submit
+  function formSubmit() {
+    // Library that holds Document Set
+    const list = 'Testlibrary';
+    const itemProperties = getSupplierDetail();
 
-  // Library that holds Document Set
-  const list = 'Testlibrary';
-  const itemProperties = getSupplierDetail();
-
-  getLastId(
-    list,
-    function(id) {
-      const docSetTitle = 'Supplier Request ' + (1000 + id);
-      createDocSetObject(list, docSetTitle, itemProperties);    
-    },
-    function(sender, args) {
-      console.log('Error:' + args.get_message());
-    }
-  );
-
-  //***
-  // Library that holds Document Set records
-  //***
-
-  
-}
-
-// Takes Document Set name and item properties to be set
-var createDocSetObject = function(list, title, item) {
-  var defer = $.Deferred();
-  // list name, Document Set title, and the Document Set's content type
-  // Data of created Document Set is returned and used to set Document Set properties
-  createDocSet(
-    list,
-    title,
-    '0x0120D520006453979D367BA7428D37D9A566C9F962'
-  ).then(
-    function(response) {
-      var folder = response.d;
-      // Save Document Set Id, eTag, and type for the update metadata call
-      item.Id = folder.Id;
-      item.eTag = folder.__metadata.etag.split('"')[1].toString();
-      var type = 'SP.Data.' + list + 'Item';
-      update(list, item, type).then(
-        function(response2) {
-          //Formulate your response to the calling funciton
-          var result = console.log('Success!');
-          defer.resolve(result);
-        },
-        function(error) {
-          defer.reject(error);
-        }
-      );
-    },
-    function(error) {
-      defer.reject(error);
-    }
-  );
-  return defer.promise;
-};
-
-var createDocSet = function(listName, folderName, folderContentTypeId) {
-  var listUrl = webUrl + '/' + listName;
-  var folderPayload = {
-    Title: folderName,
-    Path: listUrl
-  };
-
-  //Create Document Type
-  return $.ajax({
-    url: webUrl + '/_vti_bin/listdata.svc/' + listName,
-    method: 'POST',
-    contentType: 'application/json;odata=verbose',
-    data: JSON.stringify(folderPayload),
-    headers: {
-      Accept: 'application/json;odata=verbose',
-      Slug: listUrl + '/' + folderName + '|' + folderContentTypeId
-    }
-  });
-  console.log('createDocSet successful');
-};
-
-var update = function(list, item, type) {
-  var eTag = item.eTag;
-  delete item.eTag;
-  //You may need to escape the list name when setting the __metadata property "type".
-  if (type != undefined) {
-    item['__metadata'] = { type: type };
-  } else {
-    item['__metadata'] = { type: 'SP.Data.' + list + 'ListItem' };
+    getLastId(
+      list,
+      function(id) {
+        const docSetTitle = 'Supplier Request ' + (1000 + id);
+        createDocSetObject(list, docSetTitle, itemProperties);
+      },
+      function(sender, args) {
+        console.log('Error:' + args.get_message());
+      }
+    );
   }
 
-  return $.ajax({
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;odata=verbose',
-      Accept: 'application/json;odata=verbose',
-      'X-RequestDigest': document.getElementById('__REQUESTDIGEST').value,
-      'X-HTTP-Method': 'MERGE',
-      'If-Match': '"' + eTag + '"'
-    },
-    data: JSON.stringify(item),
-    url:
-      webUrl +
-      "/_api/web/lists/getbytitle('" +
-      list +
-      "')/items(" +
-      item.Id +
-      ')'
-  });
-};
-// Get form input
-function getSupplierDetail() {
-  const requestType = document.getElementById('requestType').value,
-    supplierNumber = document.getElementById('supplierNumber').value,
-    supplierName = document.getElementById('supplierName').value,
-    changeType = document.getElementById('changeType').value,
-    changeDetail = document.getElementById('changeDetail').value,
-    paymentTerms = document.getElementById('paymentTerms').value,
-    supplierType = document.getElementById('supplierType').value,
-    oneTime = document.getElementById('oneTime').checked ? 'Yes' : 'No',
-    comments = document.getElementById('comments').value;
-
-  const itemProperties = {
-    Request_x0020_Type: requestType,
-    Supplier_x0020_Number: supplierNumber,
-    Supplier_x0020_Name: supplierName,
-    Change_x0020_Type: changeType,
-    Change_x0020_Detail: changeDetail,
-    Payment_x0020_Terms: paymentTerms,
-    Supplier_x0020_Type: supplierType,
-    One_x002d_time_x0020_Supplier: oneTime,
-    Request_x0020_Comments: comments
+  // Takes Document Set name and item properties to be set
+  const createDocSetObject = function(list, title, item) {
+    var defer = $.Deferred();
+    // list name, Document Set title, and the Document Set's content type
+    // Data of created Document Set is returned and used to set Document Set properties
+    createDocSet(
+      list,
+      title,
+      '0x0120D520006453979D367BA7428D37D9A566C9F962'
+    ).then(
+      function(response) {
+        var folder = response.d;
+        // Save Document Set Id, eTag, and type for the update metadata call
+        item.Id = folder.Id;
+        item.eTag = folder.__metadata.etag.split('"')[1].toString();
+        var type = 'SP.Data.' + list + 'Item';
+        update(list, item, type).then(
+          function(response2) {
+            //Formulate your response to the calling funciton
+            var result = console.log('Success!');
+            defer.resolve(result);
+          },
+          function(error) {
+            defer.reject(error);
+          }
+        );
+      },
+      function(error) {
+        defer.reject(error);
+      }
+    );
+    return defer.promise;
   };
 
-  return itemProperties;
-}
+  var createDocSet = function(listName, folderName, folderContentTypeId) {
+    var listUrl = webUrl + '/' + listName;
+    var folderPayload = {
+      Title: folderName,
+      Path: listUrl
+    };
 
-// Get ID of last folder created
-function getLastId(list, Success, Error) {
-  var caml =
-    '<View><Query><Where>' +
-    "<Eq><FieldRef Name='FSObjType' /><Value Type='int'>1</Value></Eq>" +
-    '</Where>' +
-    "<OrderBy><FieldRef Name='ID' Ascending='False' /></OrderBy>" +
-    '</Query>' +
-    "<ViewFields><FieldRef Name='ID' /></ViewFields>" +
-    '<RowLimit>1</RowLimit>' +
-    '</View>';
-  var ctx = SP.ClientContext.get_current();
-  var web = ctx.get_web();
-  var list = web.get_lists().getByTitle(list);
-  var query = new SP.CamlQuery();
-  query.set_viewXml(caml);
-  var items = list.getItems(query);
-  ctx.load(items);
-  ctx.executeQueryAsync(function() {
-    var enumerator = items.getEnumerator();
-    enumerator.moveNext();
-    var item = enumerator.get_current();
-    id = item.get_id();
-    console.log(id);
-    Success(id);
-  }, Error);
-}
+    //Create Document Type
+    return $.ajax({
+      url: webUrl + '/_vti_bin/listdata.svc/' + listName,
+      method: 'POST',
+      contentType: 'application/json;odata=verbose',
+      data: JSON.stringify(folderPayload),
+      headers: {
+        Accept: 'application/json;odata=verbose',
+        Slug: listUrl + '/' + folderName + '|' + folderContentTypeId
+      }
+    });
+    console.log('createDocSet successful');
+  };
+
+  var update = function(list, item, type) {
+    var eTag = item.eTag;
+    delete item.eTag;
+    //You may need to escape the list name when setting the __metadata property "type".
+    if (type != undefined) {
+      item['__metadata'] = { type: type };
+    } else {
+      item['__metadata'] = { type: 'SP.Data.' + list + 'ListItem' };
+    }
+
+    return $.ajax({
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;odata=verbose',
+        Accept: 'application/json;odata=verbose',
+        'X-RequestDigest': document.getElementById('__REQUESTDIGEST').value,
+        'X-HTTP-Method': 'MERGE',
+        'If-Match': '"' + eTag + '"'
+      },
+      data: JSON.stringify(item),
+      url:
+        webUrl +
+        "/_api/web/lists/getbytitle('" +
+        list +
+        "')/items(" +
+        item.Id +
+        ')'
+    });
+  };
+  // Get form input
+  function getSupplierDetail() {
+    const requestType = document.getElementById('requestType').value,
+      supplierNumber = document.getElementById('supplierNumber').value,
+      supplierName = document.getElementById('supplierName').value,
+      changeType = document.getElementById('changeType').value,
+      changeDetail = document.getElementById('changeDetail').value,
+      paymentTerms = document.getElementById('paymentTerms').value,
+      supplierType = document.getElementById('supplierType').value,
+      oneTime = document.getElementById('oneTime').checked ? 'Yes' : 'No',
+      comments = document.getElementById('comments').value;
+
+    const itemProperties = {
+      Request_x0020_Type: requestType,
+      Supplier_x0020_Number: supplierNumber,
+      Supplier_x0020_Name: supplierName,
+      Change_x0020_Type: changeType,
+      Change_x0020_Detail: changeDetail,
+      Payment_x0020_Terms: paymentTerms,
+      Supplier_x0020_Type: supplierType,
+      One_x002d_time_x0020_Supplier: oneTime,
+      Request_x0020_Comments: comments
+    };
+
+    return itemProperties;
+  }
+
+  // Get ID of last folder created
+  function getLastId(list, Success, Error) {
+    var caml =
+      '<View><Query><Where>' +
+      "<Eq><FieldRef Name='FSObjType' /><Value Type='int'>1</Value></Eq>" +
+      '</Where>' +
+      "<OrderBy><FieldRef Name='ID' Ascending='False' /></OrderBy>" +
+      '</Query>' +
+      "<ViewFields><FieldRef Name='ID' /></ViewFields>" +
+      '<RowLimit>1</RowLimit>' +
+      '</View>';
+    var ctx = SP.ClientContext.get_current();
+    var web = ctx.get_web();
+    var list = web.get_lists().getByTitle(list);
+    var query = new SP.CamlQuery();
+    query.set_viewXml(caml);
+    var items = list.getItems(query);
+    ctx.load(items);
+    ctx.executeQueryAsync(function() {
+      var enumerator = items.getEnumerator();
+      enumerator.moveNext();
+      var item = enumerator.get_current();
+      id = item.get_id();
+      console.log(id);
+      Success(id);
+    }, Error);
+  }
+})();
 //</script>
-
