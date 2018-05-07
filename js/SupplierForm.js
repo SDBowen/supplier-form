8be202994638ab555340674621cdeab1474db980
sdbowen <sdbowen.git@gmail.com>
2018-05-06 22:19:34 -0500

    Add in Bootstrap library

---
+++ js/SupplierForm.js
@@ -0,0 +1,154 @@
+// Designed for use as a SharePoint WebPart
+//<script language="javascript" type="text/javascript">
+
+const library = 'Testlibrary';
+const webUrl =
+  window.location.protocol +
+  '//' +
+  window.location.host +
+  _spPageContextInfo.webServerRelativeUrl;
+
+// Get UI variables
+const requestTypeValue = document.getElementById('requestType'),
+  changeTypeValue = document.getElementById('changeType'),
+  uiSupplierNumber = document.getElementById('supplierNumberDiv'),
+  uiChangeType = document.getElementById('changeTypeDiv'),
+  uiChangeDetail = document.getElementById('changeDetailDiv'),
+  uiPaymentTerms = document.getElementById('paymentTermsDiv'),
+  supplierTypeValue = document.getElementById('supplierType'),
+  uiSupplierType = document.getElementById('supplierTypeDiv'),
+  uiOneTime = document.getElementById('oneTimeDiv');
+
+requestTypeValue.addEventListener('click', function() {
+  uiSupplierInput();
+});
+
+function UI() {}
+
+// Add selected files to list
+UI.prototype.addFileToList = function(fileName) {
+  const list = document.getElementById('fileList');
+  // Create tr element
+  const row = document.createElement('tr');
+  // Insert columns
+  row.innerHTML = `
+    <td>${fileName}</td>
+  `;
+
+  list.appendChild(row);
+};
+
+// DOM load event
+document.addEventListener('DOMContentLoaded', uiSupplierInput);
+
+var selDiv = '';
+var storedFiles = []; //store the object of the all files
+
+document.addEventListener('DOMContentLoaded', init, false);
+
+function init() {
+  //To add the change listener on over file element
+  document
+    .querySelector('#uploadInput')
+    .addEventListener('change', handleFileSelect, false);
+  //allocate division where you want to print file name
+  selDiv = document.querySelector('#filelist');
+}
+
+function handleFileSelect(e) {
+  // Check if file was selected
+  if (!e.target.files) return;
+
+  // Get array of file names
+  var files = e.target.files;
+  var filesArr = Array.prototype.slice.call(files);
+
+  const ui = new UI();
+
+  filesArr.forEach(function(f) {
+    //print new selected files into the given division
+    ui.addFileToList(f.name);
+  });
+}
+
+// Add form submit event listeners
+document
+  .getElementById('supplierSubmit')
+  .addEventListener('click', function(e) {
+    formSubmit();
+    e.preventDefault();
+  });
+document.getElementById('addFileButton').addEventListener('click', function(e) {
+  uploadDocument();
+  e.preventDefault();
+});
+
+// Set form input based on request type
+function uiSupplierInput() {
+  if (requestTypeValue.value === 'Update') {
+    uiPaymentTerms.style.display = 'none';
+    uiSupplierType.style.display = 'none';
+    uiOneTime.style.display = 'none';
+    uiSupplierNumber.style.display = 'block';
+    uiChangeDetail.style.display = 'block';
+    uiChangeType.style.display = 'block';
+  } else {
+    uiPaymentTerms.style.display = 'block';
+    uiSupplierType.style.display = 'block';
+    uiOneTime.style.display = 'block';
+    uiChangeDetail.style.display = 'none';
+    uiSupplierNumber.style.display = 'none';
+    uiChangeType.style.display = 'none';
+  }
+}
+
+// Create SharePoint Document Set on form submit
+// Get id number of last item in library
+function formSubmit() {
+  // Library that holds Document Set
+
+  getLastId(
+    library,
+    function(id) {
+      // Suppler detail to be passed into Document Set
+      const itemProperties = getSupplierDetail();
+      // Use last id to name new Document Set
+      id += 1000;
+      const docSetTitle = 'Supplier Request ' + id;
+      // Pass required info to create Document Set
+      createDocSetObject(library, docSetTitle, itemProperties);
+    },
+    function(sender, args) {
+      console.log('Error:' + args.get_message());
+    }
+  );
+}
+
+// Get form input
+function getSupplierDetail() {
+  const requestType = document.getElementById('requestType').value,
+    supplierNumber = document.getElementById('supplierNumber').value,
+    supplierName = document.getElementById('supplierName').value,
+    changeType = document.getElementById('changeType').value,
+    changeDetail = document.getElementById('changeDetail').value,
+    paymentTerms = document.getElementById('paymentTerms').value,
+    supplierType = document.getElementById('supplierType').value,
+    oneTime = document.getElementById('oneTime').checked ? 'Yes' : 'No',
+    comments = document.getElementById('comments').value;
+
+  const itemProperties = {
+    Request_x0020_Type: requestType,
+    Supplier_x0020_Number: supplierNumber,
+    Supplier_x0020_Name: supplierName,
+    Change_x0020_Type: changeType,
+    Change_x0020_Detail: changeDetail,
+    Payment_x0020_Terms: paymentTerms,
+    Supplier_x0020_Type: supplierType,
+    One_x002d_time_x0020_Supplier: oneTime,
+    Request_x0020_Comments: comments
+  };
+
+  return itemProperties;
+}
+
+//</script>
