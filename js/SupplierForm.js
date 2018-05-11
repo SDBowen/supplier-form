2f0046b15d8eb76679be7a158af84788193c639a
sdbowen <sdbowen.git@gmail.com>
2018-05-10 22:37:22 -0500

    Breadk out UI functions to new js file

--- js/SupplierForm.js
+++ js/SupplierForm.js
@@ -19,59 +19,38 @@ const requestTypeValue = document.getElementById('requestType'),
   uiSupplierType = document.getElementById('supplierTypeDiv'),
   uiOneTime = document.getElementById('oneTimeDiv');
 
-function UI() {}
-
-// Add selected files to list
-UI.prototype.addFileToList = function(fileName) {
-  const list = document.getElementById('fileList');
-  // Create div element
-  const row = document.createElement('div');
-  row.className = 'row mb-2';
-  // Insert columns
-  row.innerHTML = `
-    <div class="col-md-4"><p>${fileName}</p></div>
-      <div class="col-md-4">
-        <select title="Select document type(s)" class="selectpicker" data-width="100%" multiple data-selected-text-format="count">
-          <option value="ISO9001 or TS16949 Certificate">ISO9001 or TS16949 Certificate</option>
-          <option value="Certificate of Liability Insurance">Certificate of Liability Insurance</option>
-          <option value="Confidentiality Agreement">Confidentiality Agreement</option>
-          <option value="Warranty Agreement">Warranty Agreement</option>
-          <option value="Initial Supplier Questionnaire">Initial Supplier Questionnaire</option>
-          <option value="Government Contract Compliance">Government Contract Compliance</option>
-          <option value="Supplier Received Global Code of Conduct">Supplier Received Global Code of Conduct</option>
-          <option value="Financial Evaluation - D&B Report">Financial Evaluation - D&B Report</option>
-          <option value="W-9 Tax Form">W-9 Tax Form</option>
-        </select>
-      </div>
-    `;
-  // Display new files selected
-  list.appendChild(row);
-  $('.selectpicker').selectpicker('render');
-};
-
-UI.prototype.clearFileList = function() {
-  const list = document.getElementById('fileList');
-  // Clear any prior selected files
-  while (list.firstChild) {
-    list.removeChild(list.firstChild);
-  }
-};
-
-// DOM load event
-document.addEventListener('DOMContentLoaded', uiSetupForm);
-
 var selDiv = '';
 var storedFiles = []; //store the object of the all files
 
+// DOM load event
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
+  const ui = new UI();
+  ui.formSetup();
+
   //To add the change listener on over file element
   document
     .querySelector('#uploadInput')
     .addEventListener('change', handleFileSelect, false);
+
   //allocate division where you want to print file name
   selDiv = document.querySelector('#filelist');
+  document
+    .querySelector('#requestType')
+    .addEventListener('change', handleFormChange, false);
+}
+
+requestTypeValue.addEventListener('click', function() {
+  const ui = new UI();
+  ui.formSetup();
+});
+
+function handleFormChange(e) {
+  // Get array of file names
+  const ui = new UI();
+
+  ui.formType();
 }
 
 function handleFileSelect(e) {
@@ -98,38 +77,15 @@ document
   .addEventListener('click', function(e) {
     //formSubmit();
     var selectTest = document.getElementsByClassName('selectpicker');
-    let testString = "";
-    for(let i = 0; i < selectTest.length; i++) {
-      for(let e = 0; e < selectTest[i].selectedOptions.length; e++) {
+    let testString = '';
+    for (let i = 0; i < selectTest.length; i++) {
+      for (let e = 0; e < selectTest[i].selectedOptions.length; e++) {
         testString = testString + selectTest[i].selectedOptions[e].value + ', ';
+      }
     }
-  }
     console.log('testString: ' + testString);
     e.preventDefault();
   });
-
-requestTypeValue.addEventListener('click', function() {
-  uiSetupForm();
-});
-
-// Set form input based on request type
-function uiSetupForm() {
-  if (requestTypeValue.value === 'Update') {
-    uiPaymentTerms.style.display = 'none';
-    uiSupplierType.style.display = 'none';
-    uiOneTime.style.display = 'none';
-    uiSupplierNumber.style.display = 'block';
-    uiChangeDetail.style.display = 'block';
-    uiChangeType.style.display = 'block';
-  } else {
-    uiPaymentTerms.style.display = 'block';
-    uiSupplierType.style.display = 'block';
-    uiOneTime.style.display = 'block';
-    uiChangeDetail.style.display = 'none';
-    uiSupplierNumber.style.display = 'none';
-    uiChangeType.style.display = 'none';
-  }
-}
 
 // Create SharePoint Document Set on form submit
 // Get id number of last item in library
---
+++ js/formSetup.js
@@ -0,0 +1,100 @@
+//<script>
+
+function UI() {}
+
+// Add selected files to list
+UI.prototype.addFileToList = function(fileName) {
+  const list = document.getElementById('fileList');
+  // Create div element
+  const row = document.createElement('div');
+  row.className = 'row mb-2';
+  // Insert columns
+  row.innerHTML = `
+    <div class="col-md-4"><p>${fileName}</p></div>
+      <div class="col-md-4">
+        <select title="Select document type(s)" class="selectpicker" data-width="100%" multiple data-selected-text-format="count">
+          <option value="ISO9001 or TS16949 Certificate">ISO9001 or TS16949 Certificate</option>
+          <option value="Certificate of Liability Insurance">Certificate of Liability Insurance</option>
+          <option value="Confidentiality Agreement">Confidentiality Agreement</option>
+          <option value="Warranty Agreement">Warranty Agreement</option>
+          <option value="Initial Supplier Questionnaire">Initial Supplier Questionnaire</option>
+          <option value="Government Contract Compliance">Government Contract Compliance</option>
+          <option value="Supplier Received Global Code of Conduct">Supplier Received Global Code of Conduct</option>
+          <option value="Financial Evaluation - D&B Report">Financial Evaluation - D&B Report</option>
+          <option value="W-9 Tax Form">W-9 Tax Form</option>
+        </select>
+      </div>
+    `;
+  // Display new files selected
+  list.appendChild(row);
+  $('.selectpicker').selectpicker('render');
+};
+
+// Update form for selected change type
+UI.prototype.formType = function() {
+  this.formReset();
+  const changeDiv = document.getElementById('changeDetailDiv');
+
+  // Create label element
+  const changeLabel = document.createElement('label');
+  changeLabel.setAttribute('for', 'changeDetail');
+  changeLabel.innerHTML = 'This is working';    //VAR
+
+  // Create input element
+  changeInput.setAttribute('class', 'form-control');
+  const changeInput = document.createElement('input');  //VAR
+  changeInput.setAttribute('type', 'text');   //VAR
+  changeInput.setAttribute('id', 'changeDetail');   //VAR
+
+  // Display input and label
+  changeDiv.appendChild(changeLabel);
+  changeDiv.appendChild(changeInput);
+};
+
+UI.prototype.clearFileList = function() {
+  const list = document.getElementById('fileList');
+  // Clear any prior selected files
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+};
+
+UI.prototype.formReset = function() {
+  const changeDiv = document.getElementById('changeDetailDiv');
+  // Clear any prior selected files
+  while (changeDiv.firstChild) {
+    changeDiv.removeChild(changeDiv.firstChild);
+  }
+};
+
+// Set form input based on request type
+UI.prototype.formSetup = function() {
+  this.formType();
+  if (requestTypeValue.value === 'Update') {
+    uiPaymentTerms.style.display = 'none';
+    uiSupplierType.style.display = 'none';
+    uiOneTime.style.display = 'none';
+    uiSupplierNumber.style.display = 'block';
+    uiChangeDetail.style.display = 'block';
+    uiChangeType.style.display = 'block';
+  } else {
+    uiPaymentTerms.style.display = 'block';
+    uiSupplierType.style.display = 'block';
+    uiOneTime.style.display = 'block';
+    uiChangeDetail.style.display = 'none';
+    uiSupplierNumber.style.display = 'none';
+    uiChangeType.style.display = 'none';
+  }
+};
+//</script>
+
+
+const data = {
+  activeStatus: { label: 'Change Supplier to:', options: ['Active', 'Inactive'] },
+  supplierType: { label: 'Supplier Type:', options: ['Production (Certified)', 'Production (Non-Certified)', 'MRO', 'Warranty', 'Employee', 'Other'] },
+  nameChange: { label: 'New Name:' },
+  remitAddress: { label: 'New Remit Address:' },
+  paymentTerms: { label: 'Change Supplier to:', options: ['Active', 'Inactive'] },
+  paymentType: { label: 'Change Supplier to:', options: ['Active', 'Inactive'] },
+  other: { label: 'Change Description:' }
+}
